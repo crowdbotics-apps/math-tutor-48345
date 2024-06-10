c803bc8ba8c50352b19ab8c8b00383f25bb030af
Crowdbotics <team@crowdbotics.com>
2024-06-10 19:15:09 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "math-tutor-48345",
+  url: "https://math-tutor-48345.botics.co",
+  api: "https://math-tutor-48345.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
